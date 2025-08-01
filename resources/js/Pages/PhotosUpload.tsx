9d8b19beb3abcc5fcfdc60d28f920ded2f6507a5
Jesse Conner <me@jesseconner.ca>
2025-08-01 08:26:27 -0400

    Working on photos form

--- resources/js/Pages/PhotosUpload.tsx
+++ resources/js/Pages/PhotosUpload.tsx
@@ -1,6 +1,6 @@
-import type { ReactElement } from "react";
+import type { FormEvent, ReactElement } from "react";
 
-import { Head } from "@inertiajs/react";
+import { Head, useForm } from "@inertiajs/react";
 
 import type { PageProps } from "@/types";
 
@@ -15,11 +15,37 @@ import Layout from "../Layouts/MainLayout";
  * @returns The component.
  */
 export default function PhotosUpload(props: PageProps): ReactElement {
+	const { post, progress, setData } = useForm({
+		images: null as File[] | null,
+	});
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		post("/");
+	};
 	return (
 		<Layout {...props}>
 			<Head title="Add Photos" />
 			<Heading1>Add Photos</Heading1>
-			<div />
+			<form
+				className="flex items-center justify-center"
+				onSubmit={handleSubmit}
+			>
+				<label htmlFor="fileInput">Photos:</label>
+				<input
+					accept="image/png, image/jpeg"
+					id="fileInput"
+					multiple={true}
+					onChange={(e) =>
+						setData("images", e.target.files ? Array.from(e.target.files) : [])
+					}
+					type="file"
+				/>
+				{progress ? (
+					<progress max="100" value={progress.percentage}>
+						{progress.percentage}%
+					</progress>
+				) : null}
+			</form>
 		</Layout>
 	);
 }
